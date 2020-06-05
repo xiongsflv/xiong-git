21e85179878f92094531ecc7687757bc8d6d3375
xiong <1610994178@qq.com>
2020-06-05 17:12:30 +0800

    第一次提交

---
+++ src/main/java/com/tedu/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.tedu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.xml.ws.RequestWrapper;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("info")
+    public String doInfo(){
+        return "Hello huangweidong";
+    }
+}
